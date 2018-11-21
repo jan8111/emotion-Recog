4f99387cff07eb77da4f75e914975d723f265a28
jan8111 <jan8111@hotmail.com>
2018-11-20 18:00:52 -0800

    isalf

---
+++ main.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python
+
+import os
+import numpy as np
+import sys
+import caffe
+
+DEMO_DIR = ''
+categories = ['Angry', 'Disgust', 'Fear', 'Happy', 'Neutral', 'Sad', 'Surprise']
+
+cur_net_dir = 'models'
+
+input_image = caffe.io.load_image(os.path.join(DEMO_DIR,'yuanbao.png'))
+
+mean_filename=os.path.join(DEMO_DIR,cur_net_dir,'mean.binaryproto')
+proto_data = open(mean_filename, "rb").read()
+a = caffe.io.caffe_pb2.BlobProto.FromString(proto_data)
+mean  = caffe.io.blobproto_to_array(a)[0]
+
+net_pretrained = os.path.join(DEMO_DIR,cur_net_dir,'EmotiW_VGG_S.caffemodel')
+net_model_file = os.path.join(DEMO_DIR,cur_net_dir,'deploy.prototxt')
+VGG_S_Net = caffe.Classifier(net_model_file, net_pretrained,
+                       mean=mean,
+                       channel_swap=(2,1,0),
+                       raw_scale=255,
+                       image_dims=(256, 256))
+
+
+prediction = VGG_S_Net.predict([input_image],oversample=False)
+print('predicted category is {0}'.format(categories[prediction.argmax()]))
